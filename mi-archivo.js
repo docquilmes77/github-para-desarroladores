8ef4b95fdca94ff15b1c67d46f07bb918ce0c506
diego cordoba <69557798+docquilmes77@users.noreply.github.com>
2020-08-14 20:40:22 -0300

    Update mi-archivo.js

--- mi-archivo.js
+++ mi-archivo.js
@@ -1,1 +1,41 @@
-alert("EDITADO");
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Mi sitio web</title>
+	<meta charset="UTF-8" />
+	<link rel="stylesheet" type="text/css" href="stylesheets/estilos.css">
+</head>
+<body>
+		
+
+		<h1><a href="/">Tan saludable</a></h1>
+
+		<div class="contenedor">
+			
+			<div class="item">
+				<img src="/images/hamburguesas1.png" width="250">
+
+				<a href="#" class="comprar"> Comprar </a>
+			</div>
+
+			<div class="item">
+				<img src="/images/ hamburguesas2.png" width="250">
+
+				<a href="#" class="comprar"> Comprar </a>
+			</div>
+
+			<div class="item">
+				<img src="/images/hamburguesas 3.png" width="250">
+
+				<a href="#" class="comprar"> Comprar </a>
+			</div>
+
+			<div class="item">
+				<img src="/images/hamburguesas 4.png" width="250">
+
+				<a href="#" class="comprar"> Comprar </a>
+			</div>
+
+		</div>
+</body>
+</html>
